4f48984335fe59eddfadb6c6b956aa2d3ed8d537
Bill <kakekaje85@gmail.com>
2025-11-04 03:13:50 -0900

    osajdj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,105 @@ export default function Home() {
     <div className="min-h-screen bg-[#171717] flex flex-col">
       <Header />
       <Main />
-      <div className="w-full h-24"></div>
+      <Marquee className="tex">
+  
+<div className="w-[450px] h-64 bg-black rounded-2xl border border-gray-800 relative flex items-center justify-center hover:border-blue-500">
+          {/* Крутилка*/}
+          <div className="text-gray-600 relative z-0">
+            <img
+              className="hover:scale-110 duration-300 ease-in-out"
+              src="/cup-black.avif"
+              alt="cup"
+              width={200}
+              height={200}
+            />
+          </div>
+
+          {/* Кнопка поверх изображения */}
+          <div className="absolute bottom-6 left-6 flex items-center gap-3 rounded-full border border-gray-700 p-1 z-10 backdrop-blur-sm">
+            <span className="text-white font-medium px-4 py-2   rounded-full">
+              Acme Cup
+            </span>
+            <span className="text-white font-medium px-4 py-2 bg-blue-600 rounded-full">
+              $15.00 USD
+            </span>
+          </div>
+        </div>
+        {/*Крутилка дваа*/}
+        <div className="w-12"></div>
+
+        <div className="w-[450px] h-64 bg-black rounded-2xl border border-gray-800 relative flex items-center justify-center hover:border-blue-500">
+          {/* Крутилка*/}
+          <div className="text-gray-600 relative z-0">
+            <img
+              className="hover:scale-110 duration-300 ease-in-out"
+              src="/t-shirt-1.png"
+              alt="cup"
+              width={200}
+              height={200}
+            />
+          </div>
+
+          {/* Кнопка поверх изображения */}
+          <div className="absolute bottom-6 left-6 flex items-center gap-3 rounded-full border border-gray-700 p-1 z-10 backdrop-blur-sm">
+            <span className="text-white font-medium px-4 py-2   rounded-full">
+              Acme Cup
+            </span>
+            <span className="text-white font-medium px-4 py-2 bg-blue-600 rounded-full">
+              $15.00 USD
+            </span>
+          </div>
+        </div>
+
+        <div className="w-12"></div>
+
+        <div className="w-[450px] h-64 bg-black rounded-2xl border border-gray-800 relative flex items-center justify-center hover:border-blue-500">
+          <div className="text-gray-600 relative z-0">
+            <img
+              className="hover:scale-110 duration-300 ease-in-out"
+              src="/bag-1-dark.avif"
+              alt="bag"
+              width={200}
+              height={200}
+            />
+          </div>
+
+          <div className="absolute bottom-6 left-6 flex items-center gap-3 rounded-full border border-gray-700 p-1 z-10 backdrop-blur-sm">
+            <span className="text-white font-medium px-4 py-2">
+              Acme Drawstring Bag
+            </span>
+            <span className="text-white font-medium px-4 py-2 bg-blue-600 rounded-full">
+              $12.00 USD
+            </span>
+          </div>
+        </div>
+        <div className="w-12"></div>
+
+        <div className="w-[450px] h-64 bg-black rounded-2xl border border-gray-800 relative flex items-center justify-center hover:border-blue-500">
+          {/* Крутилка*/}
+          <div className="text-gray-600 relative z-0">
+            <img
+              className="hover:scale-110 duration-300 ease-in-out"
+              src="/t-shirt-1.png"
+              alt="cup"
+              width={200}
+              height={200}
+            />
+          </div>
+
+          {/* Кнопка поверх изображения */}
+          <div className="absolute bottom-6 left-6 flex items-center gap-3 rounded-full border border-gray-700 p-1 z-10 backdrop-blur-sm">
+            <span className="text-white font-medium px-4 py-2   rounded-full">
+              Acme Cup
+            </span>
+            <span className="text-white font-medium px-4 py-2 bg-blue-600 rounded-full">
+              $15.00 USD
+            </span>
+          </div>
+        </div>
+
+<div className="w-12"></div>
+</Marquee>
       <Footer />
     </div>
   );
